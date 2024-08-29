99a996e802dbb09d80229d3c76c73bb4f5b57024
aa-kr <aa-kr@mail.ru>
2024-08-29 22:56:31 +0300

    add new files

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+from app import app
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def edit_profile():
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Здесь вы можете добавить логику для обработки данных, например, сохранить их в базе данных
+
+        # Перенаправляем пользователя на ту же страницу после сохранения
+        return redirect(url_for('edit_profile'))
+
+    return render_template('edit_profile.html')
+
+
